37ff5eaaf3996dcbaab8becc2333f964bedb958c
Shivang Sharma <45060274+shivang1305@users.noreply.github.com>
2019-10-01 01:13:53 +0530

    Add files via upload

---
+++ Class_&_Inheritance.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 09:22:34 2019
+
+@author: HP
+"""
+
+class ABC:
+   
+    def show(self):
+        print("HELLO1")
+        
+    def __disp(self):
+        print("HELLO2")
+        
+a=ABC()
+a.show()
+a._ABC__disp()
+
+
+
+
+
